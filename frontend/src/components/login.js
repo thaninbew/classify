416249139af773f34a4409c1b3488c6d01d3af2d
thaninbew <kongkiatsophon.t@northeastern.edu>
2024-12-04 01:13:08 -0500

    Update login.js

--- frontend/src/components/login.js
+++ frontend/src/components/login.js
@@ -1,17 +1,9 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
 import './login.css';
 
 const Login = () => {
-  const navigate = useNavigate();
-
-  const authenticateUser = async () => {
-    try {
-      window.location.href = 'http://localhost:3001/auth/login';
-      navigate('/dashboard');
-    } catch (error) {
-      console.error('Error during authentication:', error);
-    }
+  const authenticateUser = () => {
+    window.location.href = 'http://localhost:3001/auth/login';
   };
 
   return (
