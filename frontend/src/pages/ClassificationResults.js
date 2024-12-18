7dd27765d410a8e63946ce687849a1949f2656e5
thaninbew <kongkiatsophon.t@northeastern.edu>
2024-12-18 09:00:12 -0500

    temp for classificiation pages

---
+++ frontend/src/pages/ClassificationResults.js
@@ -0,0 +1,9 @@
+const ClassificationResults = () => {
+    return (
+        <div>
+            welcome to the ClassificationResults Page. This page is the results page concluding which playlists were created.
+        </div>
+    )
+}
+
+export default ClassificationResults;
---
+++ frontend/src/pages/UserClassification.js
@@ -0,0 +1,9 @@
+const UserClassification = () => {
+    return (
+        <div>
+            welcome to the UserClassification Page. This page is for classifying genres into "buckets" within the a playlist.
+        </div>
+    )
+}
+
+export default UserClassification;
