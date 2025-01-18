4383ffc524624c4bc7d442188530b68afefbb4f9
thaninbew <kongkiatsophon.t@northeastern.edu>
2025-01-17 19:11:33 -0500

    lastfm  endpoint backend

--- backend/routes/lastFMRoute.js
+++ backend/routes/lastFMRoute.js
@@ -1,8 +1,8 @@
-
 const express = require('express');
 const router = express.Router();
 const lastFMController = require('../controllers/lastFMController');
 
 router.get('/lastfm/tags', lastFMController.getTopTags);
+router.post('/lastfm/match-and-visualize', lastFMController.matchAndVisualize);
 
 module.exports = router;
